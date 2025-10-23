1ff680ad41a7d54721cd88ba3b5396ea1587ce38
bbkbyti <bbkbyti@gmail.com>
2025-10-22 19:05:42 -0500

    created components folder and Button component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css';
+import Button from "./components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-root">
+      <header className="app-header" style={{ background: "#fff", borderBottom: `1px solid var(--border)` }}>
+        <div className="container row-spread">
+          <h1 style={{ margin: 0 }}> Expense Tracker</h1>
+        </div>
+        <div className="row">
+          <Button variant="ghost">Placeholder</Button>
+        </div>
       </header>
+
+      <main className="container">
+        <p>TEST CONTAINER</p>
+      </main>
     </div>
   );
 }
-
 export default App;
